0e039e8e85094dc773a80b4986eabf73a57017cb
AnshKaushik-05 <anshkaushik717@gmail.com>
2026-06-13 16:06:32 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int freqMap[26]={0};
+        for(char ch : s){
+            freqMap[ch-'a']++;
+        }
+        for(char ch : t){
+            freqMap[ch-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freqMap[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
