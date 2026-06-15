ca8ecd3557260cbf2dd673a3765affab747efc6a
AnshKaushik-05 <anshkaushik717@gmail.com>
2026-06-15 12:47:05 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,24 @@
+#include<map>
+#include<queue>
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+        map<int,int>freqMap;
+        priority_queue<pair<int,int>>maxHeap;
+        for(int x : nums){
+            freqMap[x]++;
+        }
+        for(auto& p : freqMap){
+            maxHeap.push({p.second,p.first});
+        }
+
+        vector<int>output;
+        while(k--){
+            pair<int,int>p=maxHeap.top();
+            maxHeap.pop();
+            output.push_back(p.second);
+        }
+        return output;
+    }
+};
