6bef49840e1ce5d887c0822521550dc9905b5b0e
AnshKaushik-05 <anshkaushik717@gmail.com>
2026-06-24 16:40:01 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,56 @@
+class node{
+public:
+    char ch;
+    unordered_map<char,node*>childMap;
+    bool terminal;
+
+    node(char ch){
+        this->ch=ch;
+        terminal=false;
+    }
+};
+
+class PrefixTree {
+public:
+    node* root;
+    PrefixTree() {
+        root=new node('#');
+    }
+    
+    void insert(string word) {
+        node* cur=root;
+        for(char x : word){
+            if(cur->childMap.find(x)==cur->childMap.end()){
+                node* n=new node(x);
+                cur->childMap[x]=n;
+            }
+            cur=cur->childMap[x];
+        }
+        cur->terminal=true;
+    }
+    
+    bool search(string word) {
+        node* cur=root;
+        for(char x : word){
+            if(cur->childMap.find(x)==cur->childMap.end()){
+                return false;
+            }
+            cur=cur->childMap[x];
+        }
+        if(cur->terminal==true){
+            return true;
+        }return false;
+    }
+    
+    bool startsWith(string prefix) {
+        node* cur=root;
+        for(char x : prefix){
+            if(cur->childMap.find(x)==cur->childMap.end()){
+                return false;
+            }
+            cur=cur->childMap[x];
+        }
+        return true;
+        
+    }
+};
