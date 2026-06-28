ff89ee3b6ccfc69c0094d190123fb234b03f7a65
AnshKaushik-05 <anshkaushik717@gmail.com>
2026-06-28 22:26:20 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,55 @@
+class node{
+public:
+    char ch;
+    bool terminal;
+    unordered_map<char,node*>childMap;
+
+    node(char ch){
+        this->ch=ch;
+        this->terminal=false;
+    }
+};
+
+class WordDictionary {
+    node* root;
+public:
+    WordDictionary() {
+        this->root=new node('#');
+    }
+    
+    void addWord(string word) {
+        node* cur=root;
+        for(char ch : word){
+            if(cur->childMap.find(ch)==cur->childMap.end()){
+                node* n=new node(ch);
+                cur->childMap[ch]=n;
+            }
+            cur=cur->childMap[ch];
+        }
+        cur->terminal=true;
+    }
+    
+    bool f(const string&word,int i,node* cur){
+        if(i==word.size()){
+            return cur->terminal;
+        }
+
+        char ch=word[i];
+        if(ch=='.'){
+            for(auto& p : cur->childMap){
+                if(f(word,i+1,p.second))return true;
+            }
+            return false;
+        }
+
+        if(cur->childMap.find(ch)==cur->childMap.end()){
+            return false;
+        }else{
+            return f(word,i+1,cur->childMap[ch]);
+        }
+
+    }
+    bool search(string word) {
+        return f(word,0,root);
+    }
+};
